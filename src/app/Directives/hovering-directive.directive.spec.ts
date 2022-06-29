07bc81068a1503aff9472314949f8727ff1583b4
AbhishekSinghTomar19 <abhishek.tomar_cs19@gla.ac.in>
2022-06-29 20:39:05 +0530

    Directive creation

---
+++ src/app/Directives/hovering-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HoveringDirectiveDirective } from './hovering-directive.directive';
+
+describe('HoveringDirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HoveringDirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HoveringDirectiveDirective } from './Directives/hovering-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HoveringDirectiveDirective
   ],
   imports: [
     BrowserModule
