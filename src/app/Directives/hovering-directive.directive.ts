561120a61517754276112b88a3b3720889e17e91
AbhishekSinghTomar19 <abhishek.tomar_cs19@gla.ac.in>
2022-06-29 21:02:45 +0530

    hovering effect completed

--- src/app/Directives/hovering-directive.directive.ts
+++ src/app/Directives/hovering-directive.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHoveringDirective]'
 })
 export class HoveringDirectiveDirective {
 
-  constructor() { }
 
+
+  constructor(private eleRef:ElementRef,private renderer:Renderer2) { }
+
+  ngOnInit(){
+    this.renderer.setStyle(this.eleRef.nativeElement,'background-color','pink');
+    this.renderer.setStyle(this.eleRef.nativeElement,'border-radius','5px');
+    this.renderer.setStyle(this.eleRef.nativeElement,'height','20px');
+    this.renderer.setStyle(this.eleRef.nativeElement,'width','200px');
+  }
+
+  @HostListener('mouseenter') mouseEnter(){
+    this.renderer.setStyle(this.eleRef.nativeElement,'background-color','aqua');
+    this.renderer.setStyle(this.eleRef.nativeElement,'border-radius','25px');
+    this.renderer.setStyle(this.eleRef.nativeElement,'height','400px');
+    this.renderer.setStyle(this.eleRef.nativeElement,'width','400px');
+  }
+
+  @HostListener('mouseleave') mouseLeave(){
+    this.renderer.setStyle(this.eleRef.nativeElement,'background-color','pink');
+    this.renderer.setStyle(this.eleRef.nativeElement,'border-radius','5px');
+    this.renderer.setStyle(this.eleRef.nativeElement,'height','20px');
+    this.renderer.setStyle(this.eleRef.nativeElement,'width','200px');
+  }
 }
